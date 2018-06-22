360cce846b022ec72aa73b9fc2af201ac0dca28d
clyy <clyy.chenl@gmail.com>
2018-06-22 15:03:40 +0800

    新增全局配置节点

--- src/main/java/com/clyy/entity/GlobalConfig.java
+++ src/main/java/com/clyy/entity/GlobalConfig.java
@@ -2,8 +2,19 @@ package com.clyy.entity;
 
 /**
  * Auther: Charles.Chen <br>
- * Description:
+ * Description: 全局配置
  * Date: Create in 14:18 2018/6/22
  **/
 public class GlobalConfig {
+
+	// 是否进行执行前的确认流程
+	private boolean executeConfirm = true;
+
+	public boolean isExecuteConfirm() {
+		return executeConfirm;
+	}
+
+	public void setExecuteConfirm(boolean executeConfirm) {
+		this.executeConfirm = executeConfirm;
+	}
 }
