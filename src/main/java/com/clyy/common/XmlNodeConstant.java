7bbb37453cb4c839c3b236bcb6a4a254ccedb310
clyy <clyy.chenl@gmail.com>
2018-06-22 15:02:23 +0800

    添加编译打包配置

---
+++ src/main/java/com/clyy/common/XmlNodeConstant.java
@@ -0,0 +1,9 @@
+package com.clyy.common;
+
+/**
+ * Auther: Charles.Chen <br>
+ * Description:
+ * Date: Create in 14:46 2018/6/22
+ **/
+public class XmlNodeConstant {
+}
---
+++ src/main/java/com/clyy/entity/GlobalConfig.java
@@ -0,0 +1,9 @@
+package com.clyy.entity;
+
+/**
+ * Auther: Charles.Chen <br>
+ * Description:
+ * Date: Create in 14:18 2018/6/22
+ **/
+public class GlobalConfig {
+}
